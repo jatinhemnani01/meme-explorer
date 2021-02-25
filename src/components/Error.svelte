9d8554f5447b9568c27404cc3c162a966e4c4f2d
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-02-25 21:26:26 +0530

    fixed snackbar

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -6,7 +6,7 @@
 {#if $isError}
   <div
     in:fly={{ duration: 400, y: 300 }}
-    out:fly={{ duration: 400, y: -200 }}
+    out:fly={{ duration: 300, y: -200 }}
     id="snackbar"
   >
     Subreddit Doesn't Exist
@@ -14,26 +14,19 @@
 {/if}
 
 <style>
-  /* The snackbar - position it at the bottom and in the middle of the screen */
   #snackbar {
-    font-size: 1.2em;
-    visibility: visible; /* Hidden by default. Visible on click */
-    min-width: 250px; /* Set a default minimum width */
-    margin-left: -125px; /* Divide value of min-width by 2 */
-    background-color: #f23333; /* Black background color */
-    color: #fff; /* White text color */
-    text-align: center; /* Centered text */
-    border-radius: 2px; /* Rounded borders */
-    padding: 16px; /* Padding */
-    position: fixed; /* Sit on top of the screen */
-    z-index: 1; /* Add a z-index if needed */
-    left: 50%; /* Center the snackbar */
-    bottom: 30px; /* 30px from the bottom */
+    visibility: visible;
+    min-width: 250px;
+    background-color: #e03838;
+    color: #fff;
+    text-align: center;
+    border-radius: 2px;
+    padding: 16px;
+    z-index: 1;
+    bottom: 30px;
+    font-size: 17px;
   }
 
-  /* Show the snackbar when clicking on a button (class added with JavaScript) */
-
-  /* Animations to fade the snackbar in and out */
   @-webkit-keyframes fadein {
     from {
       bottom: 0;
